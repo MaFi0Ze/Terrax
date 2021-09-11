3a3caeb946a7725446d0c24fb81b4ded7bf0c5fa
MaFi0Ze <84603329+MaFi0Ze@users.noreply.github.com>
2021-09-11 14:24:56 +0400

    Add files via upload

---
+++ install-.sh
@@ -0,0 +1,9 @@
+pkg install git curl wget nmap -y 
+ wget https://github.com/Hax4us/TermuxBlack/raw/master/install.sh
+ chmod +x install.sh 
+ ./install.sh -i 
+ pkg install ruby2 
+ wget https://raw.githubusercontent.com/Hax4us/Metasploit_termux/master/metasploit.sh 
+ chmod +x metasploit.sh 
+ ./metasploit.sh
+echo Done
